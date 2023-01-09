d189d945ff5c86ab18d9cff868f40a06fb733929
Makson Vinicio <maksonvinicio7@gmail.com>
2023-01-09 14:24:14 -0300

    Inicializando estrutura do projeto

---
+++ create_db.py
@@ -0,0 +1,18 @@
+from database import engine, Base
+
+from models.model import Base
+
+
+print('Criando database')
+
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
+
+
+def drop_all():
+    Base.metadata.drop_all(bind=engine)
+
+
+if __name__ == '__main__':
+    create_db()
