2d9225f2b061cd210a8939e0a71ddcdd866be5b5
Makson Vinicio <maksonvinicio7@gmail.com>
2023-01-10 13:04:11 -0300

    Criando bucket com terraform gcp

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = "1.2.8"
+  required_version = "1.3.7"
 }
 
 provider "google" {
   project     = var.project_id
   region      = var.region_central
   zone        = var.zone_central
-  credentials = file("")
+  credentials = file("${path.cwd}/secrets_key.json")
 }
 
 resource "google_storage_bucket" "i4sea_bucket" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,24 @@
 variable "project_id" {
   type    = string
-  default = "starry-sylph-349013 "
+  default = "dulcet-legend-332817"
+}
+
+variable "region_west" {
+  type    = string
+  default = "us-west1"
+}
+
+variable "region_central" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone_west" {
+  type    = string
+  default = "us-west1-b"
+}
+
+variable "zone_central" {
+  type    = string
+  default = "us-central1-b"
 }
